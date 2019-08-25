d9d1bead8ecdbb29a107c93066152aac445f5258
sgoryunov <sgoryunov@loginet.ru>
2019-08-25 21:31:11 +0300

    upd reducer

--- src/store/timers/reducer.js
+++ src/store/timers/reducer.js
@@ -1,12 +1,42 @@
 const initialState = {
-    topicsByUrl: {},
-    selectedTopicUrls: []
+    timers: [],
+    activeTimer: null
 }
 
-export default function reduce(state = initialState, action = {}) {
-    switch (action.type) {
-      default:
-        return state;
+export default function reduce(state = initialState, {type, payload}) {
+    switch (type) {
+        case 'FETCH_TIMERS' :
+            return {
+                ...state,
+                timers: [...state.timers]
+            }
+        case 'ADD_TIMER' : 
+            return {
+                ...state,
+                timers: [...state.timers, state.payload]
+            }
+        case 'CHANGE_TIMER' : 
+            return {
+                ...state,
+                timers: state.timers.map(timer => {
+                    if(timer.id === payload.id){
+                        return { ...payload }
+                    }
+                    return timer
+                })
+            }
+        case 'REMOVE_TIMER' : 
+            return {
+                ...state,
+                timers: state.timers.filter(timer => timer.id !== payload)
+            }
+        case 'TIMER_LOADING' : 
+            return {
+                ...state, 
+                isLoading: payload
+            }
+        default:
+            return state;
     }
 }
 
