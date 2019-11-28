1d09698919e18f7996ba546142575d578bcef205
GSE995 <gorynovse@gmail.com>
2019-11-28 18:55:08 +0300

    added combine reducers

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,9 @@
 import React from 'react'
+import store from './store'
 import { render } from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
 import App from './App'
-import reducers from './store/timers/reducer'
 import { BrowserRouter } from 'react-router-dom'
-
-const store = createStore(reducers, applyMiddleware(thunk))
 
 render(
     (<BrowserRouter>
