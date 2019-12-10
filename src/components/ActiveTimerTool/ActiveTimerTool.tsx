c3d141eddfa7e58b0f074654c4220f620f7bf65e
GSE995 <gorynovse@gmail.com>
2019-12-11 00:54:57 +0300

    moved styled component from ActiveTimerTool

--- src/components/ActiveTimerTool/ActiveTimerTool.tsx
+++ src/components/ActiveTimerTool/ActiveTimerTool.tsx
@@ -3,53 +3,17 @@ import { addTimer } from '../../store/timers/actions'
 import { Timer } from '../../models'
 import { connect } from 'react-redux'
 import { AppState } from '../../store/'
-import styled from 'styled-components'
 
-const Wrapper = styled.div`
-    height: 66px;
-    display: flex;
-    box-shadow: rgba(0, 0, 0, 0.13) 0px 2px 6px 0px;
-    margin-bottom: 20px;
-`
+import {
+    Wrapper,
+    Icon,
+    TimerActions,
+    TimerNameField,
+    TimerNameWrapper,
+    TimerValue,
+    TriggerButton
+} from './styled'
 
-const TimerActions = styled.div`
-    display: flex;
-    justify-content: right;
-    margin-right: 20px;
-`
-
-const TimerNameWrapper = styled.div`
-    height: 100%;
-    flex-grow: 1;
-    padding-left: 10px;
-`
-
-const TimerNameField = styled.input`
-    height: 100%;
-    padding: 0;
-    margin: 0;
-    width: 100%;
-    border: none;
-    font-family: 'Roboto', sans-serif;
-    font-size: 1.2em;
-    &:focus {
-        outline: none;
-    }
-`
-
-const Icon = styled.i((props: any) => ({
-    'font-size': props.size,
-    color: props.color,
-}))
-
-const TimerValue = styled.div`
-    height: 100%;
-    line-height: 66px;
-    margin-right: 10px;
-    font-size: 1.5em;
-`
-
-const TriggerButton = styled.button``
 
 type ActiveTimerToolProps = {
     addTimer: Function
@@ -168,7 +132,7 @@ class ActiveTimerTool extends Component<
                     />
                 </TimerNameWrapper>
                 <TimerActions>
-                    <TimerValue >{displayTimerValue}</TimerValue>
+                    <TimerValue>{displayTimerValue}</TimerValue>
                     <TriggerButton onClick={triggerButtonHandler}>
                         <Icon {...triggerIcon} />
                     </TriggerButton>
