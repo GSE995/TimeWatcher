e6d3504f372fa53177ef8952d7bac6e413e71bd8
GSE995 <gorynovse@gmail.com>
2019-12-11 01:27:30 +0300

    added result in TimerService

--- src/services/TimersService.ts
+++ src/services/TimersService.ts
@@ -1,55 +1,63 @@
-import Timer, {ITimerDTO} from '../models/Timer'
-import Result from '../models/Result'
+import axios from 'axios'
+import Timer, { TimerDTO } from '../models/Timer'
+import {Result, SuccessResult, ErrorResult} from '../models/Result'
 import ListResult from '../models/ListResult'
 import PageSize from '../models/PageSize'
 
 const API_ROOT_URL = '/api/timer'
 export { API_ROOT_URL }
+
 // сервис заглушка
-export default class TimersServie {
-    
-    static get(id: number): Promise<Result<Timer>> {
-        return new Promise((resolve, reject) => {
-            resolve({
-                data: new Timer('newe'),
-                success: true,
-                message: '',
-            })
-        })
-    }
+export default class TimersServie<T> {
 
-    static async create(timer: Timer): Promise<Timer> {
-        const params: RequestInit = {
-            method: 'POST',
-            body: JSON.stringify(timer)
+    static async get(id: number): Promise<SuccessResult<Timer> | ErrorResult> {
+        try {
+            let {data} = await axios.get(`${API_ROOT_URL}/${id}`)
+            return new SuccessResult(Timer.createFrom(data))
+        } catch (error) {
+            console.log(error)
+            return new ErrorResult(error)
         }
-        let response = await fetch(`${API_ROOT_URL}`, params)
-        let {data} = await response.json()
-        return Timer.createFrom(data)
     }
 
-    static async save(timer: Timer): Promise<Timer> {
-        const params: RequestInit = {
-            method: 'PUT',
-            body: JSON.stringify(timer)
+    static async create(timer: Timer): Promise<SuccessResult<Timer> | ErrorResult> {
+        try {
+            let {data} = await axios.post(`${API_ROOT_URL}`, timer)
+            return new SuccessResult(Timer.createFrom(data))
+        } catch (error) {
+            console.log(error)
+            return new ErrorResult(error)
         }
-        return fetch(`${API_ROOT_URL}`, params)
-                                .then(res => res.json())
-        
     }
 
-    static async remove(id: number): Promise<Result<any>> {
-        const params: RequestInit = {
-            method: 'DELETE'
+    static async save(timer: Timer): Promise<SuccessResult<Timer> | ErrorResult> {
+        try {
+            let response  = await axios.put(`${API_ROOT_URL}`, timer)
+            return new SuccessResult(timer)
+        } catch (error) {
+            console.log(error)
+            return new ErrorResult(error)
         }
-        return fetch(`${API_ROOT_URL}/${id}`, params)
-                                .then(res => res.json())
     }
 
-    static async getList(pageSize: PageSize): Promise<ListResult<Timer>> {
-        let response = await fetch(`${API_ROOT_URL}/list`)
-        let {data, total} = await response.json()
-        let timers = data.map((el: ITimerDTO ) => Timer.createFrom(el))
-        return new ListResult(timers, total, pageSize)
+    static async remove(id: number): Promise<SuccessResult<Boolean> | ErrorResult> {
+        try {
+            let response = await axios.delete(`${API_ROOT_URL}/${id}`)
+            return new SuccessResult(true)
+        } catch (error) {
+            console.log(error)
+            return new ErrorResult(error)
+        }
+    }
+
+    static async getList(pageSize: PageSize): Promise<SuccessResult<ListResult<Timer[]>> | ErrorResult> {
+        try {
+            let {result} = await axios.get(`${API_ROOT_URL}/list`)
+            let listResult = new ListResult<Timer[]>(result.data, result.total, pageSize)
+            return new SuccessResult(listResult)
+        } catch (error) {
+            console.log(error)
+            return new ErrorResult(error)
+        }
     }
 }
