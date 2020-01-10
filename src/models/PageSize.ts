59951fbcf4fe2274dbdc6e3e69da5bd62a965b74
GSE995 <gorynovse@gmail.com>
2020-01-10 03:29:38 +0300

    set default page size

--- src/models/PageSize.ts
+++ src/models/PageSize.ts
@@ -2,7 +2,7 @@ export default class PageSize {
     start: number
     limit: number
 
-    constructor(start: number, limit: number) {
+    constructor(start: number = 0, limit: number = 25) {
         this.start = start
         this.limit = limit
     }
