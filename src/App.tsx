0a59ef56991b05ff65919de20514f88301afcb0c
GSE995 <gorynovse@gmail.com>
2020-01-31 01:33:05 +0300

    wrapp pages  ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,32 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import styled from 'styled-components'
+import { ErrorBoundary } from './common/ErrorBoundary'
 
 import Menu from './components/Menu/Menu'
 import routes from './routes'
 
 const AppContainer = styled.div`
-    height: 100%;
-    display: flex;
+  height: 100%;
+  display: flex;
 `
 const PageWrapper = styled.div`
-    flex-grow: 1;
+  flex-grow: 1;
 `
 
 function App() {
+  const routeComponents = routes.map(el => <Route {...el} />)
 
-    const routeComponents = routes.map((el) => (
-        <Route {...el} />
-    ))
-    
-    return (
-        <AppContainer>
-            <Menu routes={routes} />
-            <PageWrapper>
-                <Switch>
-                    {routeComponents}
-                </Switch>
-            </PageWrapper>
-        </AppContainer>
-    )
+  return (
+    <AppContainer>
+      <Menu routes={routes} />
+      <PageWrapper>
+        <ErrorBoundary>
+          <Switch>{routeComponents}</Switch>
+        </ErrorBoundary>
+      </PageWrapper>
+    </AppContainer>
+  )
 }
 
 export default App
