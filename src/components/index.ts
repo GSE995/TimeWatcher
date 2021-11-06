4f0e3e33e6f53afcf4e54d3bd4198a770b86857a
GSE995 <gorynovse@gmail.com>
2021-11-06 20:05:50 +0300

    added export new components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export * from './ActiveTimerTool/ActiveTimerTool';
 export * from './TimerCard/TimerCard';
 export * from './TimersBlock/TimersBlock';
+export * from './GroupedTimers/GroupedTimers';
+export * from './ErrorBoundary/ErrorBoundary';
