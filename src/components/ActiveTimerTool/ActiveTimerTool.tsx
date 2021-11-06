6ba4bfa2746515ecd86336521c9b27ab9e6e525c
GSE995 <gorynovse@gmail.com>
2021-11-06 20:07:24 +0300

    remove other components from ActiveTimerTools

--- src/components/ActiveTimerTool/ActiveTimerTool.tsx
+++ src/components/ActiveTimerTool/ActiveTimerTool.tsx
@@ -1,123 +1,54 @@
-import React, { FC, useCallback, useEffect, useRef, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { FC, useCallback, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import cn from 'classnames';
 
 import { stopTimer } from '../../store/timers/asyncActions';
 import { startTimer as addActiveTimer } from '../../store/timers/actions';
 import { Timer } from '../../models';
-import { tickTime, getDisplayTimerValue } from '../../utils/timer';
-import TimersService from '../../services/TimerServiceFirebase';
-import { getTimerValue } from '../../models/Timer';
 import { Button } from '../Button/Button';
+import { selectActiveTimer } from '../../store/timers/selectors';
+import { TimerViewer } from '../TimerViewer/TimerViewer';
+import TimersService from '../../services/TimerServiceFirebase';
 
 import css from './ActiveTimerTool.module.scss';
 
-interface DisplayTimerProps {
-  timer: Timer;
-}
-
-export const DisplayTimer: FC<DisplayTimerProps> = ({ timer }) => {
-  const localTimerValue = useRef(new Date(0));
-  const [timerValue, setTimerValue] = useState(new Date(0));
-  const [intervalId, setIntervalId] = useState(null);
-
-  const tick = useCallback(() => {
-    const tickT = tickTime(localTimerValue.current);
-    localTimerValue.current = tickT;
-    setTimerValue(tickT);
-  }, []);
-
-  useEffect(() => {
-    if (timer.id && timer.id !== timer.id) {
-      clearInterval(intervalId as any);
-      setTimerValue(new Date(0));
-      setIntervalId(null);
-      localTimerValue.current = new Date(0);
-
-      setIntervalId(setInterval(() => tick(), 1000) as any);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [timer]);
-
-  return <div className={css.TimerValue}>{getDisplayTimerValue(timerValue)}</div>;
-};
+const emptyTimer = new Timer();
 export interface ActiveTimerToolProps {
   timer: Timer;
 }
 
-export const ActiveTimerTool: FC<ActiveTimerToolProps> = ({ timer }) => {
-  const [localTimer, setLocalTimer] = useState(timer);
-  const [timerValue, setTimerValue] = useState(new Date(0));
-  const [intervalId, setIntervalId] = useState(null);
-  const localTimerValue = useRef(new Date(0));
+export const ActiveTimerTool: FC = () => {
+  const timer: Timer = useSelector(selectActiveTimer);
 
-  const dispatch = useDispatch();
+  const [localTimer, setLocalTimer] = useState(emptyTimer);
 
-  const onNameChange = useCallback(
-    (e: any) => {
-      setLocalTimer({ ...localTimer, name: e.target.value });
-    },
-    [localTimer]
-  );
-
-  const tick = useCallback(() => {
-    const tickT = tickTime(localTimerValue.current);
-    localTimerValue.current = tickT;
-    setTimerValue(tickT);
+  const onNameChange = useCallback((e: any) => {
+    setLocalTimer(state => ({ ...state, name: e.target.value }));
   }, []);
 
+  const dispatch = useDispatch();
   const onStart = useCallback(async () => {
     const startDate = new Date();
-
-    setIntervalId(setInterval(() => tick(), 1000) as any);
 
     TimersService.create({ ...localTimer, startDate }).then(savedTimer => {
       setLocalTimer({ ...localTimer, id: savedTimer.id, startDate });
       dispatch(addActiveTimer(savedTimer));
     });
-  }, [dispatch, localTimer, tick]);
+  }, [dispatch, localTimer]);
 
   const onStop = useCallback(() => {
-    clearInterval(intervalId as any);
-
-    const stoppedTimer = { ...timer, endDate: new Date() };
+    const stoppedTimer = { ...localTimer, endDate: new Date() };
     dispatch(stopTimer(stoppedTimer));
 
     setLocalTimer(new Timer());
-
-    setTimerValue(new Date(0));
-    setIntervalId(null);
-
-    localTimerValue.current = new Date(0);
-  }, [dispatch, intervalId, timer]);
+  }, [dispatch, localTimer]);
 
   useEffect(() => {
-    if (timer.id && timer.id !== localTimer.id) {
+    if (timer?.id && timer.id !== localTimer.id) {
       setLocalTimer(timer);
-
-      clearInterval(intervalId as any);
-      setTimerValue(getTimerValue(timer));
-      setIntervalId(null);
-      localTimerValue.current = getTimerValue(timer);
-
-      setIntervalId(setInterval(() => tick(), 1000) as any);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [timer]);
-
-  useEffect(() => {
-    if (timer.id) {
-      setLocalTimer(timer);
-
-      clearInterval(intervalId as any);
-      setTimerValue(getTimerValue(timer));
-      setIntervalId(null);
-      localTimerValue.current = getTimerValue(timer);
-
-      setIntervalId(setInterval(() => tick(), 1000) as any);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   return (
     <div className={css.ActiveTimerTool}>
@@ -132,9 +63,9 @@ export const ActiveTimerTool: FC<ActiveTimerToolProps> = ({ timer }) => {
         />
       </div>
       <div className={css.Actions}>
-        <div className={css.TimerValue}>{getDisplayTimerValue(timerValue)}</div>
-        <Button onClick={intervalId ? onStop : onStart}>
-          <i className={cn('fas', intervalId ? 'fa-play-circle' : 'fa-stop' )}></i>
+        <TimerViewer timer={timer} />
+        <Button onClick={timer ? onStop : onStart}>
+          <i className={cn('fas', timer ? 'fa-stop' : 'fa-play-circle')}></i>
         </Button>
       </div>
     </div>
