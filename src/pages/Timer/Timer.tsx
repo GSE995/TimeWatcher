237b16cf54e188dfd7a726b98c5c61779e2dd75f
GSE995 <gorynovse@gmail.com>
2021-11-06 20:09:05 +0300

    remove unused code in Timer page

--- src/pages/Timer/Timer.tsx
+++ src/pages/Timer/Timer.tsx
@@ -1,39 +1,12 @@
-import React, { useEffect, Fragment, useMemo } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { Fragment } from 'react';
 
-import { fetchTimers } from '../../store/timers/asyncActions';
-import { ActiveTimerTool, TimersBlock } from '../../components';
-import { PageSize, Timer } from '../../models';
-import { groupByDate } from '../../utils/timer';
-
-function useTimers(pageSize: PageSize) {
-  let dispatch = useDispatch();
-  let timers = useSelector((state: any) => state.timer.timers);
-
-  useEffect(() => {
-    if (!timers.length) {
-      dispatch(fetchTimers(pageSize));
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return timers;
-}
+import { ActiveTimerTool, GroupedTimers } from '../../components';
 
 export const TimerPage = () => {
-  let timers = useTimers(new PageSize(0, 10));
-  let timer = useSelector((state: any) => state.timer.activeTimer || new Timer());
-
-  let grouped = useMemo(() => groupByDate(timers, 'endDate'), [timers]);
-
-  if (timers.length === 0) return <></>;
-
   return (
     <Fragment>
-      <ActiveTimerTool timer={timer} />
-      {grouped.map(el => (
-        <TimersBlock timers={el} key={+el[0].startDate!} />
-      ))}
+      <ActiveTimerTool />
+      <GroupedTimers />
     </Fragment>
   );
 };
